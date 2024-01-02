4a7a6ed1a83dfaf3026cbb4763a689a8a405a3fc
Jedi830 <jdelacruz@brooklynsteamcenter.org>
2024-01-02 10:51:24 -0500

    Dude perfect is a FRAUD

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,8 +6,16 @@ todos = ["Play Lego fortnite,","Work out consistently"]
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    new_todo = request.form["new_todo"]
-    todos.append = (new_todo)
+    if request.method == 'POST':
+        new_todo = request.form["new_todo"]
+        todos.append(new_todo)
     return render_template("jtodo.html.jinja", js_todos = todos)
+
+
+@app.route('/delete_todo/<int:todo_index>', methods = ['POST'])
+def todo_delete(todo_index):
+    del todos[todo_index]
+    return redirect('/')
+
 
 
